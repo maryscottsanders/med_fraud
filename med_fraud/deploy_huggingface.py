a3f859e8cc91d9a6eeed11451a3f59bf2bccd136
maryscottsanders <maryscottsanders@gmail.com>
2021-08-14 14:03:25 -0400

    add flake8

--- med_fraud/deploy_huggingface.py
+++ med_fraud/deploy_huggingface.py
@@ -4,48 +4,48 @@
 
 # Resources
 # https://sagemaker.readthedocs.io/en/stable/
-# https://github.com/huggingface/notebooks/blob/master/sagemaker/11_deploy_model_from_hf_hub/deploy_transformer_model_from_hf_hub.ipynb
+# https://github.com/huggingface/notebooks/blob/master/sagemaker/11_deploy_model_from_hf_hub/deploy_transformer_model_from_hf_hub.ipynb  # noqa
 # https://huggingface.co/blog/deploy-hugging-face-models-easily-with-amazon-sagemaker
 
-from sagemaker.huggingface.model import HuggingFaceModel #Need .model
+from sagemaker.huggingface.model import HuggingFaceModel  # Need .model
 import sagemaker
 
 role = sagemaker.get_execution_role()
 
-# Hub Model configuration. https://huggingface.co/models
+#  Hub Model configuration. https://huggingface.co/models
 hub = {
-  'HF_MODEL_ID':'distilbert-base-uncased-distilled-squad', # model_id from hf.co/models
-  'HF_TASK':'question-answering' # NLP task you want to use for predictions
+  'HF_MODEL_ID': 'distilbert-base-uncased-distilled-squad',  # model_id from hf.co/models
+  'HF_TASK': 'question-answering'  # NLP task you want to use for predictions
 }
 
-# create Hugging Face Model Class
+#  Create Hugging Face Model Class
 huggingface_model = HuggingFaceModel(
    env=hub,
-   role=role, # iam role with permissions to create an Endpoint
-   transformers_version="4.6", # transformers version used
-   pytorch_version="1.7", # pytorch version used
-   py_version="py36", # python version of the DLC
+   role=role,  # iam role with permissions to create an Endpoint
+   transformers_version="4.6",  # transformers version used
+   pytorch_version="1.7",  # pytorch version used
+   py_version="py36",  # python version of the DLC
 )
 
-# deploy model to SageMaker Inference
+# Deploy model to SageMaker Inference
 predictor = huggingface_model.deploy(
    initial_instance_count=1,
    instance_type="ml.m5.xlarge"
 )
 
 
-# example request, you always need to define "inputs"
+# Example request, you always need to define "inputs"
 data = {
-"inputs": {
-    "question": "What is used for inference?",
-    "context": "My Name is Philipp and I live in Nuremberg. This model is used with sagemaker for inference."
-    }
-}
+         "inputs": {
+                     "question": "What is used for inference?",
+                     "context": "My Name is Philipp and I live in Nuremberg. This model is used with sagemaker for inference."  # noqa
+                     }
+         }
 
-# request
+# Eequest
 predictor.predict(data)
 
 
 breakpoint()
-# delete endpoint
+# Delete endpoint
 predictor.delete_endpoint()
